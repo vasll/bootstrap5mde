a5ea359401ff011603d93cbad984c20e5d415182
vasll <valentinosoll17@gmail.com>
2023-03-07 15:34:37 +0100

    Added tooltips Added mdi-preview-off/on icons

--- js/bootstrap5mde.js
+++ js/bootstrap5mde.js
@@ -2,12 +2,13 @@ $(document).ready(()=>{
     let isCurrentMarkdownRaw = true;    // true if showing raw textarea, false if showing parsed markdown
 
     $('#btn-switch-markdown').on('click', ()=>{
-        console.log(isCurrentMarkdownRaw)
         if(isCurrentMarkdownRaw == true){
+            $('#img-switch-markdown').attr('src', 'img/mdi-preview-on.svg')
             $('#bootstrap5mde-raw').hide()
             $('#bootstrap5mde-parsed').html(marked.parse($('#bootstrap5mde-raw').val()))    // TODO sanitize parsing
             $('#bootstrap5mde-parsed').show()
         }else if(isCurrentMarkdownRaw == false){
+            $('#img-switch-markdown').attr('src', 'img/mdi-preview-off.svg')
             $('#bootstrap5mde-parsed').hide()
             $('#bootstrap5mde-raw').show()
         }
