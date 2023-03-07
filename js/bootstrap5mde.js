cffb0d46cb8afa9f2d6c790c5de191cb61593f43
vasll <valentinosoll17@gmail.com>
2023-03-07 15:05:17 +0100

    Renamed project to bootstrap5mde

--- js/bootstrap5mde.js
+++ js/bootstrap5mde.js
@@ -1,16 +1,15 @@
 $(document).ready(()=>{
-
     let isCurrentMarkdownRaw = true;    // true if showing raw textarea, false if showing parsed markdown
 
     $('#btn-switch-markdown').on('click', ()=>{
         console.log(isCurrentMarkdownRaw)
         if(isCurrentMarkdownRaw == true){
-            $('#romanianmd-raw').hide()
-            $('#romanianmd-parsed').html(marked.parse($('#romanianmd-raw').val()))
-            $('#romanianmd-parsed').show()
+            $('#bootstrap5mde-raw').hide()
+            $('#bootstrap5mde-parsed').html(marked.parse($('#bootstrap5mde-raw').val()))
+            $('#bootstrap5mde-parsed').show()
         }else if(isCurrentMarkdownRaw == false){
-            $('#romanianmd-parsed').hide()
-            $('#romanianmd-raw').show()
+            $('#bootstrap5mde-parsed').hide()
+            $('#bootstrap5mde-raw').show()
         }
 
         isCurrentMarkdownRaw = !isCurrentMarkdownRaw    // toggle isCurrentMarkdownRaw
